07565e2952ffaccbf7ae0f798e5a963a47c0af09
ahmetbahadircol@gmail.com <ahmetbahadircol@gmail.com>
2022-01-22 17:25:23 +0300

    Item Location model is created. Stock Movements model is created.

--- stracker/locations/models.py
+++ stracker/locations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models import PROTECT
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import BaseAbstractModel
+from items.models import Items
+from shelves.models import ConnectionUserStorages
+
+
+class ItemLocations(models.Model):
+    """
+    commend
+    """
+    connection = models.ForeignKey(ConnectionUserStorages, verbose_name=_("Connection"), on_delete=PROTECT)
+    item = models.ForeignKey(Items, verbose_name=_("Items"), on_delete=PROTECT)
+    quantity = models.PositiveIntegerField(_("quantity"), null=False, blank=True, default=0)
+
+
+class StockMovements(BaseAbstractModel):
+    """
+    commend
+    """
+    direction = models.IntegerField(_("direction"), null=False, blank=False)
+    item_location = models.ForeignKey(ItemLocations, verbose_name=_("item location"), on_delete=PROTECT)
+    quantity = models.PositiveIntegerField(_("quantity"), null=False, blank=True, default=0)
+
