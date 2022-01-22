e0db14b7d6a07d7cf01eaf28b92825704990e533
ahmetbahadircol@gmail.com <ahmetbahadircol@gmail.com>
2022-01-22 16:02:56 +0300

    Shelves model is created.

--- stracker/shelves/models.py
+++ stracker/shelves/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Shelves(models.Model):
+    """
+    commend
+    """
+    number = models.PositiveIntegerField(_("number"), null=False, blank=False)
+    description = models.CharField(_("description"), max_length=250, null=False, blank=False)
+    location = models.CharField(_("location"), max_length=250, null=False, blank=False)
+    status = models.BooleanField(_("status"), null=False, blank=False)
+
+    class Meta:
+        verbose_name = _("shelve")
+        verbose_name_plural = _("shelves")
+
+    def __str__(self):
+        return f"{self.number} {self.description}"
+
