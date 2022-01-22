26886c11a1c791a9655e8ec4c3a03af7befcc7ef
ahmetbahadircol@gmail.com <ahmetbahadircol@gmail.com>
2022-01-22 16:59:40 +0300

    Connection User Storages model is created.

--- stracker/shelves/models.py
+++ stracker/shelves/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.db.models import PROTECT
 from django.utils.translation import gettext_lazy as _
+
+from users.models import User
 
 
 class Shelves(models.Model):
     """
-    commend
+    Shelves class based model
     """
     number = models.PositiveIntegerField(_("number"), null=False, blank=False)
     description = models.CharField(_("description"), max_length=250, null=False, blank=False)
@@ -17,4 +20,14 @@ class Shelves(models.Model):
 
     def __str__(self):
         return f"{self.number} {self.description}"
+
+
+class ConnectionUserStorages(models.Model):
+    """
+    Many-To-Many relationship between shelves and users
+    Class based model
+    """
+    user = models.ForeignKey(User, verbose_name=_("user"), on_delete=PROTECT)
+    shelve = models.ForeignKey(Shelves, verbose_name=_("shelve"), on_delete=PROTECT)
+
 
